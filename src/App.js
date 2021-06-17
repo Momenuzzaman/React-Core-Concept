426d1b8fe8406804d6efb39a36be12dfe9a8cd20
mdemon420 <mdeamon4@gmail.com>
2021-06-17 20:05:09 +0600

    component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
+       <p>React</p>
+       <Person name="Kamal khondokar" job="student" ></Person>
+       <Person name="Rabbi Hassan" job="football"></Person>
+       
       </header>
     </div>
   );
+}
+function Person(props){
+  let personalStyle = {
+    border: "3px solid white",
+    padding:"15px",
+    margin: "10px"
+    
+  }
+ return (
+   <div style = {personalStyle}>
+     <h1>Name: {props.name}</h1>
+     <p>Profetion: {props.job} </p>
+   </div>
+ )
 }
 
 export default App;
